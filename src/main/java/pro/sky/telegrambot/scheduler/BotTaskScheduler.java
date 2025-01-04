b5c3977b8bac606642954929871f3ddf3e8f0910
tychk <semarooney@yandex.ru>
2025-01-04 15:30:06 +0300

    Telegram bot with updates to review

--- src/main/java/pro/sky/telegrambot/scheduler/BotTaskScheduler.java
+++ src/main/java/pro/sky/telegrambot/scheduler/BotTaskScheduler.java
@@ -2,7 +2,7 @@ package pro.sky.telegrambot.scheduler;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import pro.sky.telegrambot.util.NotificationService;
+import pro.sky.telegrambot.service.NotificationService;
 
 @Component
 public class BotTaskScheduler {
--- src/main/java/pro/sky/telegrambot/service/NotificationService.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationService.java
@@ -1,14 +1,12 @@
-package pro.sky.telegrambot.util;
+package pro.sky.telegrambot.service;
 
 import com.pengrad.telegrambot.request.SendMessage;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.listener.TelegramBotUpdatesListener;
 import pro.sky.telegrambot.model.NotificationTask;
 import pro.sky.telegrambot.repository.TaskRepository;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
