a98030c0da6b614b93d2ec259528cdcf9aeccce4
tychk <semarooney@yandex.ru>
2025-01-04 14:49:58 +0300

    Telegram bot is ready to review

--- target/classes/liquibase/scripts/task.sql
+++ target/classes/liquibase/scripts/task.sql
@@ -5,7 +5,6 @@ CREATE TABLE if not exists "notification_task"
 (
     ID                BIGSERIAL PRIMARY KEY,
     CHAT_ID           VARCHAR   NOT NULL,
-    REQUEST           VARCHAR   NOT NULL,
     MESSAGE           VARCHAR   NOT NULL,
     NOTIFICATION_DATE TIMESTAMP NOT NULL,
     CREATION_DATE     TIMESTAMP NOT NULL
